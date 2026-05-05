d6a80d89f764da823c6a7301ed62669eb15a55c0
intellicore555-oss <intellicore555@gmail.com>
2026-05-05 15:48:42 -0300

    Atualizar o visitas.js

--- api/visitas.js
+++ api/visitas.js
@@ -1,6 +1,11 @@
 let visitas = 0;
 
 export default function handler(req, res) {
+  // 🔥 libera acesso externo
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
   visitas++;
+
   res.status(200).json({ visitas });
 }
