7ec716b26ef2f5112d98f5d8717b5bf156d9947a
intellicore555-oss <intellicore555@gmail.com>
2026-05-05 16:11:30 -0300

    Atualizar o visitas.js

--- api/visitas.js
+++ api/visitas.js
@@ -1,11 +1,29 @@
-let visitas = 0;
+import { MongoClient } from "mongodb";
 
-export default function handler(req, res) {
-  // 🔥 libera acesso externo
+const uri = "SUA_URL_AQUI";
+const client = new MongoClient(uri);
+
+export default async function handler(req, res) {
+  await client.connect();
+
+  const db = client.db("meusite");
+  const collection = db.collection("contador");
+
+  let doc = await collection.findOne({ name: "visitas" });
+
+  if (!doc) {
+    doc = { name: "visitas", value: 0 };
+    await collection.insertOne(doc);
+  }
+
+  doc.value++;
+
+  await collection.updateOne(
+    { name: "visitas" },
+    { $set: { value: doc.value } }
+  );
+
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
 
-  visitas++;
-
-  res.status(200).json({ visitas });
+  res.status(200).json({ visitas: doc.value });
 }
