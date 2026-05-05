6fc5597183cd4fad11d380050e113bfcb2e3c6c9
intellicore555-oss <intellicore555@gmail.com>
2026-05-05 16:33:07 -0300

    Atualizar o visitas.js

--- api/visitas.js
+++ api/visitas.js
@@ -1,44 +1,4 @@
-import { MongoClient } from "mongodb";
-
-const uri = "mongodb+srv://intellicore555_db_user:299792458anjinho%40@cluster0.gnh9bcl.mongodb.net/meusite?retryWrites=true&w=majority";
-
-let client;
-let clientPromise;
-
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri);
-  global._mongoClientPromise = client.connect();
-}
-
-clientPromise = global._mongoClientPromise;
-
-export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  try {
-    const client = await clientPromise;
-
-    const db = client.db("meusite");
-    const collection = db.collection("contador");
-
-    let doc = await collection.findOne({ name: "visitas" });
-
-    if (!doc) {
-      doc = { name: "visitas", value: 0 };
-      await collection.insertOne(doc);
-    }
-
-    doc.value++;
-
-    await collection.updateOne(
-      { name: "visitas" },
-      { $set: { value: doc.value } }
-    );
-
-    res.status(200).json({ visitas: doc.value });
-
-  } catch (error) {
-    console.error("ERRO:", error);
-    res.status(500).json({ erro: "Erro no servidor" });
-  }
+catch (error) {
+  console.log(error);
+  res.status(500).json({ erro: error.message });
 }
